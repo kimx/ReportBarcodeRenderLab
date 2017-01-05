1f6f140febbc8f7ff8def16e3dcff6e01ab38731
User <User@KIM-MSI>
2017-01-05 16:06:54 +0800

    Add project files.

---
+++ ReportBarcodeRenderLab/BarcodeProxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+namespace ReportBarcodeRenderLab
+{
+    public class BarcodeProxy
+    {
+        private BarcodeLib.Barcode Barcode;
+        public BarcodeProxy()
+        {
+            this.Barcode = new BarcodeLib.Barcode();
+        }
+        public byte[] Render(string value)
+        {
+            Barcode.Alignment = BarcodeLib.AlignmentPositions.CENTER;
+            Barcode.LabelPosition = BarcodeLib.LabelPositions.BOTTOMCENTER;
+            Barcode.IncludeLabel = true;
+            Barcode.AlternateLabel = value;
+            Barcode.RotateFlipType = RotateFlipType.Rotate180FlipXY;
+            var image = Barcode.Encode(BarcodeLib.TYPE.CODE128A, value, Color.Black, Color.White, 250, 90);
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ ReportBarcodeRenderLab/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReportBarcodeRenderLab.Default" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=12.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+ 
+        <asp:Button ID="btnRender" runat="server" Text="Render" OnClick="btnRender_Click" />
+
+        <rsweb:ReportViewer ID="rv" runat="server" Height="532px" Width="800px">
+        </rsweb:ReportViewer>
+
+    </form>
+</body>
+</html>
